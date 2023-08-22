bbb54d10b3fd6720872e94bfee28fa96a12382fb
AlganOngun <alganunlu1896@gmail.com>
2023-08-22 15:51:54 +0300

    Added core functionality

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
+const QAs = [
+	{
+		question: "Test Sorusu 1",
+		answer: "Test Yaniti 1"
+	},
+	{
+		question: "Test Sorusu 2",
+		answer: "Test Yaniti 2"
+	},
+	{
+		question: "Test Sorusu 3",
+		answer: "Test Yaniti 3"
+	},
+	{
+		question: "Test Sorusu 4",
+		answer: "Test Yaniti 4"
+	}
+];
+
 let i = 0;
 const speed = 50;
 const messagesElement = document.getElementById('messages');
+
+(function runSession(j) {
+	if(j < QAs.length) setTimeout(function() {
+		runSession(j);
+		clearSession();
+	}, 5000);
+	else {
+		clearSession();
+		runSession(0);
+	}
+
+	startQA(QAs[j]);
+	j++;
+
+} (0));
+
+function startQA(qa) {
+	typeQuestion(document.getElementById("userQuestion"), qa.question, qa);
+}
+
+function clearSession() {
+	messagesElement.innerHTML = "";
+}
 
 function typeMessage(messageElement, currentText) {
   if (i < currentText.length) {
@@ -9,11 +51,26 @@ function typeMessage(messageElement, currentText) {
     i++;
     setTimeout(() => typeMessage(messageElement, currentText), speed);
   } else {
+		i = 0;
     messageElement.innerHTML = currentText;
   }
 }
 
-function askQuestion() {
+function typeQuestion(messageElement, currentText, currentQA) {
+  if (i < currentText.length) {
+    messageElement.value =
+      currentText.substring(0, i);
+    i++;
+    setTimeout(() => typeQuestion(messageElement, currentText, currentQA), speed);
+  } else {
+		i = 0;
+    messageElement.value = currentText;
+    setTimeout(() => submitQuestion(currentQA), 1000);
+  }
+}
+
+
+function submitQuestion(currentQA) {
   const userQuestion = document.getElementById('userQuestion').value;
   if (userQuestion.trim() === '') return;
 
@@ -24,7 +81,7 @@ function askQuestion() {
 
   document.getElementById('userQuestion').value = '';
 
-  const response = getResponse(userQuestion);
+  const response = currentQA.answer;
   const botMessage = document.createElement('div');
   botMessage.className = 'bot';
   messagesElement.appendChild(botMessage);
@@ -34,11 +91,4 @@ function askQuestion() {
   }, 1000);
 
   messagesElement.scrollTop = messagesElement.scrollHeight;
-}
-
-function getResponse(question) {
-  if (question.includes('merhaba')) return 'Merhaba!';
-  if (question.includes('nasılsın'))
-    return 'İyiyim, teşekkür ederim. Sen nasılsın?';
-  return 'Anlamadım, lütfen başka bir şey sorun.';
 }
